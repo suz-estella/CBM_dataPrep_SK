21640ba8857c0dfefb074f5a529420660925cfce
Celine <cboisvenue@gmail.com>
2024-10-17 13:29:15 -0700

    remove the (HEAD) call for CBMutils

--- CBM_dataPrep_SK.R
+++ CBM_dataPrep_SK.R
@@ -15,7 +15,7 @@ defineModule(sim, list(
   documentation = list("CBM_dataPrep_SK.Rmd"),
   reqdPkgs = list(
     "data.table", "terra","fasterize", "magrittr", "raster", "RSQLite", "sf",
-    "PredictiveEcology/CBMutils@celineTest (HEAD)",
+    "PredictiveEcology/CBMutils@development",
     "PredictiveEcology/LandR@development"
   ),
   parameters = rbind(
