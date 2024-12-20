025001b2550b7acefd6c2424843e05e1266f818d
Susan Murray <murray.e.susan@gmail.com>
2024-12-20 07:43:52 -0800

    reproducible.inputPaths option now only changed if not already set

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -21,9 +21,11 @@ withr::defer({
 # Set reproducible options:
 # - Use a shared input data directory
 # - Silence messaging
-withr::local_options(
-  list(reproducible.inputPaths = c(testDirs$temp$inputs, getOption("reproducible.inputPaths"))),
-  .local_envir = teardownEnv)
+if (is.null(getOption("reproducible.inputPaths"))){
+  withr::local_options(
+    list(reproducible.inputPaths = testDirs$temp$inputs),
+    .local_envir = teardownEnv)
+}
 if (testthat::is_testing()) withr::local_options(list(reproducible.verbose = -2), .local_envir = teardownEnv)
 
 # Set Require package options:
